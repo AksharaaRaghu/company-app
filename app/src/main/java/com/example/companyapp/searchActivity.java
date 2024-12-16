6677b2af5e985bfe22f812fb70bcc3181cfd4eac
AksharaaRaghu <akshararaghu2004@gmail.com>
2024-12-16 22:31:37 +0530

    project in progress

---
+++ app/src/main/java/com/example/companyapp/searchActivity.java
@@ -0,0 +1,34 @@
+package com.example.companyapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class searchActivity extends AppCompatActivity {
+    Button b1,b2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_search);
+
+        b1=(Button) findViewById(R.id.search);
+        b2=(Button) findViewById(R.id.backto);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ob=new Intent(getApplication(),menuActivity.class);
+                startActivity(ob);
+            }
+        });
+
+    }
+}
